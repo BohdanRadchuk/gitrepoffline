af01f600be29a274c38336e16f996b6323f9ad2f
zaalovich <zaalovich@icloud.com>
2017-09-04 21:39:45 +0300

    изменил

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Это ёлочка");
+        System.out.println("Этоhjhjhhkhjfhggh лочка");
         System.out.println("       /\\    " ) ;
         System.out.println("      /  \\   " ) ;
         System.out.println("     / || \\ ");
